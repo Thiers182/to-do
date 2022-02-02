f84c26ccd116704fd19ee0496247661593bac626
Thiers Souza <thier_s182@hotmail.com>
2022-02-02 15:16:27 -0300

    commit_message

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,31 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      done: false, // por padrão uma task digitada não pode estar concluída inicialmente, não tem lógica
+      title: newTaskTitle
+    }
+
+    setTasks(oldTasks => [...oldTasks, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
+    const updateTasks = tasks.map(task => ({...task}))
+
+    const foundItem = updateTasks.find(item => item.id === id)
+
+    if (!foundItem)
+      return;
+    
+    foundItem.done = !foundItem.done;
+    setTasks(updateTasks)
     //TODO - toggle task done if exists
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updateTasks = tasks.filter(task => task.id !== id); // Aqui vai ser feito um filtro onde o retorno vai ser uma lista com todos as tasks diferentes daquela que foi selecionada
+    setTasks(updateTasks);
   }
 
   return (
